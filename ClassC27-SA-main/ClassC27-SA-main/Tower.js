00dfa3020bc648be41ccc4f1953b26de5d442f7f
jojozinhoblox <125510663+jojozinhoblox@users.noreply.github.com>
2023-04-19 18:24:08 -0300

    Add files via upload

---
+++ ClassC27-SA-main/ClassC27-SA-main/Tower.js
@@ -0,0 +1,9 @@
+class Tower{
+    constructor(x,y,w,h){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        World.add(world,this.body)
+    }
+}
